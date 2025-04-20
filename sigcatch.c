b26736cdf4a3b1ebb411030287362ca96d4c2b5b
Agustin Palma <aguspalqui@hotmail.com>
2025-04-19 21:15:41 -0300

    sigcatch hecho

--- sigcatch.c
+++ sigcatch.c
@@ -1,7 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>     // para printf()
+#include <stdlib.h>    // para exit()
+#include <signal.h>    // para señales
+#include <string.h>    // para strsignal()
+#include <unistd.h>    // para pause()
 
-int main(int argc, char *argv[])
-{
-    exit(EXIT_SUCCESS);
+
+// funcion que se ejecuta cuando llega una señal
+void handler(int signum) {
+
+    // imprime el número de la señal y su descripcion
+    printf("%d: %s\n", signum, strsignal(signum));
+
+    fflush(stdout); 
+}
+
+int main(int argc, char *argv[]) {
+
+    
+    // estructura para manejar señales
+    struct sigaction sa;
+    sa.sa_handler = handler; // funcion que maneja la señal    
+    sa.sa_flags = 0;                
+    sigemptyset(&sa.sa_mask); // esto es para no bloquear ninguna señal mientras se ejecute el proceso
+
+         
+
+    // registra el manejador handler() para todas las señales 
+    for (int i = 1; i < _NSIG; i++) {        
+        sigaction(i, &sa, NULL);    
+    }
+
+   // el proceso se suspende hasta que llegue una señal
+    while (1) {
+        pause();
+    }
+
+    return EXIT_SUCCESS;
 }
